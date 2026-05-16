4fd1065ac639c7e9caebdb27cfc68b888c485bca
unknown <Gustavo.silva123913@gmail.com>
2026-05-16 13:31:30 -0300

    Adicionei o template ao registro e iniciando a integração do back end ao front

--- app/routes/auth_route.py
+++ app/routes/auth_route.py
@@ -1,5 +1,5 @@
 from flask.blueprints import Blueprint
-from flask import request, jsonify, session, Response
+from flask import request, jsonify, session, Response, render_template, redirect
 from app.services.auth_service import Autenticacao
 from app.models.user import UserRepository
 
@@ -11,18 +11,16 @@ banco = UserRepository()
 @auth_bp.route('/cadastro', methods=['POST'])
 def cadastrar_usuario() -> tuple[Response, int]:
     if request.method == 'POST':
-        dados = request.get_json()
-        if not dados:
-            return jsonify({"Erro": "Body JSON é obrigatório"}), 400
-        nome = dados.get('nome')
-        email = dados.get('email')
-        senha = dados.get('senha')
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
         if not nome or not email or not senha:
             return jsonify({"Erro": "Todos os campos devem estar preenchidos"}), 400
         resposta, mensagem = autenticacao.cadastrar_usuario(nome, email, senha)
         if not resposta:
             return jsonify({"Erro": mensagem}), 400
         return jsonify({"Sucesso": mensagem}), 201
+    return render_template('registro.html')
 
 
 @auth_bp.route('/login', methods=['POST'])
