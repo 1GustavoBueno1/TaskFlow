fd1afca5c716cf2d7253ede99f5102364d57daac
unknown <Gustavo.silva123913@gmail.com>
2026-05-30 20:49:17 -0300

    Preparando para o primeiro deploy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from app import create_app
+import os
 app = create_app()
 if __name__ == '__main__':
-    app.run(debug = True)
+    debug = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
+    app.run(debug = debug)
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import os
+from waitress import serve
+from app import create_app
+app = create_app()
+if __name__ == '__main__':
+    host = os.getenv('HOST', '0.0.0.0')
+    port = int(os.getenv('PORT', '8000'))
+    serve(app, host = host, port = port)
