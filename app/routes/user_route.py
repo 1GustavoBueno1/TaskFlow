82fd639ed82d34ec88cd103e697829919156ceb0
unknown <Gustavo.silva123913@gmail.com>
2026-06-22 17:51:59 -0300

    Adicionando decorator de login a user

--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -3,51 +3,43 @@ from flask import request, session, Response, render_template, redirect, url_for
 from app.models.user import UserRepository
 from app.services.auth_service import Autenticacao
 from app.services.user_service import Usuario
-
+from app.decorators import login_necessario
 usuario_bp = Blueprint('usuario', __name__, url_prefix='/perfil')
 banco_usuario = UserRepository()
 servico_usuario = Usuario()
 servico_auth = Autenticacao()
 @usuario_bp.route('/visualizar', methods=['GET'])
+@login_necessario
 def visualizar_perfil() -> Response | str:
     id_usuario = session.get('user_id')
-    if id_usuario:
-        usuario = banco_usuario.find(id_usuario)
-        return render_template('perfil.html', dados_do_perfil = usuario)
-    return redirect(url_for('homepage'))
+    usuario = banco_usuario.find(id_usuario)
+    return render_template('perfil.html', dados_do_perfil = usuario)
 
 
 @usuario_bp.route('/editar/<coluna>', methods=['GET', 'POST'])
+@login_necessario
 def editar_perfil(coluna) -> str | Response:
-    if request.method == 'POST':
-        id_usuario = session.get('user_id')
-        if id_usuario:
-            novo_dado = request.form.get(coluna)
-            user_senha = request.form.get('password1')
-            usuario = banco_usuario.find(id_usuario)
-            resposta, mensagem = servico_usuario.editar_perfil(coluna, novo_dado, user_senha, id_usuario, usuario)
-            if resposta:
-                flash(mensagem, 'sucesso')
-                return redirect(url_for('usuario.visualizar_perfil'))
-            flash(mensagem, 'erro')
-            return redirect(url_for('usuario.visualizar_perfil'))
-        flash("efetue login para prosseguir", 'erro')
-        return redirect(url_for('homepage'))
+    id_usuario = session.get('user_id')
+    novo_dado = request.form.get(coluna)
+    user_senha = request.form.get('password1')
+    usuario = banco_usuario.find(id_usuario)
+    resposta, mensagem = servico_usuario.editar_perfil(coluna, novo_dado, user_senha, id_usuario, usuario)
+    if resposta:
+        flash(mensagem, 'sucesso')
+        return redirect(url_for('usuario.visualizar_perfil'))
     return render_template('edit_user.html', coluna = coluna)
 @usuario_bp.route('/sair', methods = ['GET', 'POST'])
+@logout_necessario
 def sair() -> Response | str:
+    id_usuario = session.get('user_id')
     if request.method == 'POST':
-        id_usuario = session.get('user_id')
-        if id_usuario:
-            senha_user = request.form.get('password')
-            usuario = banco_usuario.find(id_usuario)
-            resposta, mensagem = servico_auth.sair(senha_user, usuario)
-            if resposta:
-                session.clear()
-                flash(mensagem, 'sucesso')
-                return redirect(url_for('homepage'))
-            flash(mensagem, 'erro')
-            return redirect(url_for('usuario.visualizar_perfil'))
-        flash('Efetue login para prosseguir', 'erro')
-        return redirect(url_for('homepage'))
+        senha_user = request.form.get('password')
+        usuario = banco_usuario.find(id_usuario)
+        resposta, mensagem = servico_auth.sair(senha_user, usuario)
+        if resposta:
+            session.clear()
+            flash(mensagem, 'sucesso')
+            return redirect(url_for('homepage'))
+        flash(mensagem, 'erro')
+        return redirect(url_for('usuario.visualizar_perfil'))
     return render_template('logout.html')
